1c18599f43e36f0b9f1d84fcc56d1ea1202e9a32
ZePotente <ezeposada@gmail.com>
2020-12-22 16:00:54 -0300

    Armada prueba inicial.

--- Grua.f90
+++ Grua.f90
@@ -7,20 +7,32 @@ PROGRAM GRUA
     
     REAL(8), DIMENSION(:,:), ALLOCATABLE :: A
     REAL(8), DIMENSION(:), ALLOCATABLE :: B, XGAUSS, XGJ, XCROUT
-    INTEGER :: BANDERAA, BANDERAB, BANDERA
+    INTEGER :: BANDERAA, BANDERAB, BANDERA !Banderas reutilizables.
     CHARACTER(*), PARAMETER :: FORMATO = '(F25.15)' !Es el mismo formato del módulo, pero por si se quiere cambiar.
     
+    !Prueba inicial
+    PRINT *, 'Lectura de los datos iniciales.'
     CALL MAT_LEER(A, BANDERAA, 'A.txt')
+    CALL MAT_LEER(ACROUT, BANDERAC, 'ACrout.txt')
     CALL VEC_LEER(B, BANDERAB, 'B.txt')
     
-    IF (.NOT. VERIF_LECT(BANDERAA, BANDERAB)) GOTO 20
-    CALL MOSTRAR_DATOS(A, B)
+    IF (.NOT. VERIF_ARCH(BANDERAA, BANDERAB, BANDERAC)) GOTO 20
+    PRINT *, 'Datos leidos correctamente.'
+    CALL MOSTRAR_DATOS(A, ACROUT, B)
     
-    CALL PRUEBA_METODOS_DIR(A, B, XGAUSS, XGJ, XCROUT)
-    !Se guardan los resultados de la prueba
-    CALL VEC_GUARDAR(XGAUSS, BANDERA, 'Resultados por Gauss.txt')
-    CALL VEC_GUARDAR(XGJ, BANDERA, 'Resultados por Gauss-Jordan.txt')
-    CALL VEC_GUARDAR(XCROUT, BANDERA, 'Resultados por LUCrout.txt')
+    PRINT *, 'Prueba de métodos en la posición inicial.'
+    CALL PRUEBA_METODOS_DIR(A, ACROUT, B, XGAUSS, XGJ, XCROUT)
+    PRINT *, 'Métodos finalizados.'
+    
+    PRINT *, 'Guardando resultados en archivo...'
+    CALL VEC_GUARDAR(XGAUSS, BANDERAA, 'Resultados por Gauss.txt')
+    CALL VEC_GUARDAR(XGJ, BANDERAB, 'Resultados por Gauss-Jordan.txt')
+    CALL VEC_GUARDAR(XCROUT, BANDERAC, 'Resultados por LUCrout.txt')
+    
+    IF (.NOT. VERIF_ARCH(BANDERAA, BANDERAB, BANDERAC)) GOTO 20 
+    PRINT *, 'Resultados guardados correctamente.'
+    
+    
     
     GOTO 10
 20  PRINT *, 'Error al leer de archivo.'
@@ -28,24 +40,27 @@ PROGRAM GRUA
 10  PRINT *, 'Fin del programa.'
 CONTAINS
 
-    SUBROUTINE MOSTRAR_DATOS(A, B)
-        REAL(8), INTENT(IN) :: A(:,:), B(:)
+    SUBROUTINE MOSTRAR_DATOS(A, ACROUT, B)
+        REAL(8), INTENT(IN) :: A(:,:), ACROUT(:,:) B(:)
         !
         CHARACTER(*), PARAMETER :: FORMATO = '(F10.4)'
         
         PRINT *, 'Matriz inicial:'
         CALL MAT_MOSTRAR(A, FORMATO)
         
+        PRINT *, 'Matriz inicial para método de Crout:'
+        CALL MAT_MOSTRAR(ACROUT, FORMATO)
+        
         PRINT *, 'Vector de términos independientes:'
         CALL VEC_MOSTRAR(B, FORMATO)
     
     END SUBROUTINE
     
-    FUNCTION VERIF_LECT(BA, BB)
+    FUNCTION VERIF_ARCH(BA, BB, BC)
         LOGICAL :: VERIF_LECT
-        INTEGER, INTENT(IN) :: BA, BB
+        INTEGER, INTENT(IN) :: BA, BB, BC
         
-        VERIF_LECT = (BA /= 1) .AND. (BB /= 1)
+        VERIF_LECT = (BA /= 1) .AND. (BB /= 1) .AND. (BC /= 1)
     END FUNCTION
     
     !Se devuelven los resultados obtenidos por los diferentes métodos directos.
